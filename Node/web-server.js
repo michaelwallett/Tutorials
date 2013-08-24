fcf0e645c7fd7036ae6c1050ada426dcc3dc2b0f
Michael Wallett <michael.wallett@gmail.com>
2013-08-24 12:27:05 +0100

    setup hello express

---
+++ Node/web-server.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  res.write("Hello \n")
+
+  setTimeout(function() {
+  	  res.end('World\n');
+  }, 2000);
+
+}).listen(1337, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:1337/');
